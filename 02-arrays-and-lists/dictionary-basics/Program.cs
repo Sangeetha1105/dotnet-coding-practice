8d9b7e4a5e68292fba423b72dded4dbfd199d704
Sangeetha1105 <62664622+Sangeetha1105@users.noreply.github.com>
2026-03-25 23:33:27 +0100

    feat: implement arrays, list operations, and dictionary usage (#2)
    Program:
    Implemented array operations:
    Printing elements
    Iterating using loops and foreach
    Implemented List operations:
    Add and AddRange
    Remove and RemoveAt
    Contains check
    Filtering using LINQ (even numbers)
    Implemented Dictionary basics:
    Adding key-value pairs
    Iterating through dictionary
    Accessing values using keys
    Checking key existence using ContainsKey

---
+++ 02-arrays-and-lists/dictionary-basics/Program.cs
@@ -0,0 +1,35 @@
+﻿// Q1: Create a dictionary:
+// Key: int (Id)
+// Value: string (Name)
+// 👉 Add:
+// (1, "Sangeetha")
+// (2, "Rahul")
+// 👉 Print all key-value pairs
+
+// Q2: Check if key exists before accessing:
+// 👉 If key 1 exists → print value
+// 👉 Else → print "Not found"
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Dictionary<int, string> employee = new Dictionary<int, string>();
+        employee.Add(1, "Sangeetha");
+        employee.Add(2, "Rahul");
+
+        if (employee.ContainsKey(2))
+        {
+            Console.WriteLine(employee[2]); // correct
+        }
+        else
+        {
+            Console.WriteLine("Not found");
+        }
+        foreach (var emp in employee)
+        {
+            Console.WriteLine(emp.Key + "-" + emp.Value);
+        }
+    }
+}
