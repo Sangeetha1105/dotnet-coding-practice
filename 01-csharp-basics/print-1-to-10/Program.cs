9cee0c7ed81188474150f40c708c8c2c2aac9fae
Sangeetha1105 <62664622+Sangeetha1105@users.noreply.github.com>
2026-03-25 21:22:01 +0100

    feat: add basic loop practice questions (#1)
    program:
    Even or Odd number check
    Print numbers from 1 to 10
    Print even numbers from 1 to 20
    Sum of numbers from 1 to 10

---
+++ 01-csharp-basics/print-1-to-10/Program.cs
@@ -0,0 +1,10 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        for (int i = 1; i <= 10; i ++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
---
+++ 01-csharp-basics/sum-1-to-10/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int sum=0;
+        for (int i = 1; i <= 10; i ++)
+        {
+            sum=sum+i;
+        }
+        Console.WriteLine(sum);
+    }
+}
+
